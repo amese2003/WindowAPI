d57a31fd928ec67e53efdbb4fca638fe7f187851
용주 곽 <amese1225@gmail.com>
2023-01-26 22:18:08 +0900

    Elipse

--- ApiPractice/ApiPractice/ApiPractice.cpp
+++ ApiPractice/ApiPractice/ApiPractice.cpp
@@ -159,6 +159,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            Ellipse(hdc, 20, 20, 80, 80);
             
             EndPaint(hWnd, &ps);
         }
